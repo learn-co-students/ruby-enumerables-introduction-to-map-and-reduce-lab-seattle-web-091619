26078c41cac224ec58a9b2447a7c89f8f87fa09a
Cole Ditzler <colejditzler@gmail.com>
2019-09-13 10:51:01 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,111 @@
 # My Code here....
+def map_to_no_change (array)
+
+  array
+end
+
+def map_to_negativize (array)
+  a = 0
+  while (array[a]) do
+  array[a] = array[a] * -1
+  a = a+1
+  end
+
+
+
+
+    array
+  end
+
+
+def map_to_square (array)
+a = 0
+while (array[a]) do
+array[a] = array[a]**2
+a = a+1
+end
+
+
+
+
+  array
+end
+
+def map_to_double (array)
+  a = 0
+  while (array[a]) do
+  array[a] = array[a]*2
+  a = a+1
+  end
+
+
+
+
+    array
+  end
+
+  def MyOwnReduceLikeMethod (array)
+
+
+
+
+    end
+
+def reduce_to_total (array, sp=0)
+  a = 0
+  b = sp
+  while (array[a]) do
+  b = array[a]+ b
+  array[a] = b
+  a = a+1
+  end
+
+
+
+
+    b
+  end
+
+
+def reduce_to_all_true (array)
+  a = 0
+  b = 0
+  while (a < array.length) do
+ if !array[a]
+return false
+end
+  a = a+1
+  end
+
+
+
+array
+  end
+
+
+def reduce_to_any_true (array)
+  a = 0
+  b = 0
+  while (a < array.length) do
+ if array[a]
+return true
+end
+  a = a+1
+  end
+
+
+
+false
+  end
+
+
+def map_to_no_change (array)
+
+array
+end
+
+
+def map_to_no_change (array)
+
+array
+end
